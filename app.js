20800019254ae8436eeb4acf654f558536119dca
alexisabena <32970506+alexisabena@users.noreply.github.com>
2022-05-26 22:26:40 -0500

    Adding Js

---
+++ app.js
@@ -0,0 +1,8 @@
+const rating = document.getElementById('rating-form');
+console.log(rating);
+
+const ratings = document.getElementsByClassName('visible')
+console.log(ratings);
+
+const form = document.querySelector('form');
+console.log(form);
