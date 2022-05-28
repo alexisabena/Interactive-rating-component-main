c5b888a2bf2f10ba13542995d4629eb91ee0e2e5
alexisabena <32970506+alexisabena@users.noreply.github.com>
2022-05-27 23:03:23 -0500

    DOM exercises

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const rating = document.getElementById('rating-form');
-console.log(rating);
+// const title = document.getElementById('mainTitle');
+// console.log(ratingForm);
 
-const ratings = document.getElementsByClassName('visible')
-console.log(ratings);
+// const ratingsRes = document.getElementsByClassName('circle-value')
+// console.log(ratingsRes);
 
-const form = document.querySelector('form');
-console.log(form);
+// const form = document.getElementsByTagName('input');
+// console.log(form);
+
+
+// const valueRating = document.querySelectorAll('#5');
+// console.log(valueRating);
+
+const title = document.querySelectorAll('#mainTitle');
+
+console.log(title);
+title.style.color = 'blue';
